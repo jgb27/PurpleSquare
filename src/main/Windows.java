a2f6f303be40aa4f8ff49bf159aec6c2107293a1
João Gustavo Soares Bispo <joaogustavobispo15@gmail.com>
2024-05-27 23:16:11 -0300

    chore: Refactor Windows class for better readability and maintainability

--- src/main/Windows.java
+++ src/main/Windows.java
@@ -5,16 +5,16 @@ import java.awt.Dimension;
 
 public class Windows {
 
+    // Construtor
     public Windows(int width, int height, String title, Game game) {
-        JFrame frame = new JFrame(title);
-        frame.setPreferredSize(new Dimension(width, height));
-        frame.setMaximumSize(new Dimension(width, height));
-        frame.setMinimumSize(new Dimension(width, height));
-
-        frame.add(game);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+        JFrame frame = new JFrame(title); // Cria a janela do jogo
+        frame.setPreferredSize(new Dimension(width, height)); // Define o tamanho da janela
+        frame.setMaximumSize(new Dimension(width, height)); // Define o tamanho máximo da janela
+        frame.setMinimumSize(new Dimension(width, height)); // Define o tamanho mínimo da janela
+        frame.add(game); // Adiciona o jogo na janela
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Define o que acontece quando a janela é fechada
+        frame.setResizable(false); // Define se a janela pode ser redimensionada
+        frame.setLocationRelativeTo(null); // Define a posição da janela
+        frame.setVisible(true); // Define se a janela é visível
     }
 }
